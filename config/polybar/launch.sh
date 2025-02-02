9a1741b6528a47b96ab45e59a99da0ecbf72d976
videogamelynx <Dajokerr68@protonmail.com>
2025-02-02 17:57:47 +0000

    i3 update

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -5,6 +5,7 @@ THEME="lofi"
 killall polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-CONFIG_DIR=$(dirname $0)/themes/$THEME/config.ini
+# CONFIG_DIR=$(dirname $0)/themes/$THEME/config.ini
+CONFIG_DIR=/home/guts/.config/polybar/themes/lofi/config.ini
 polybar main -c $CONFIG_DIR &
 polybar secondary -c $CONFIG_DIR &
